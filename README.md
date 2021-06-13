cace639c7fcb09cd6c2e57ec9e2c64479583d346
Anggara Yuda Pratama <anggara.yuda04@gmail.com>
2021-06-13 23:04:48 +0700

    Creating CV

--- README.md
+++
@@ -1,2 +0,0 @@
-# CV_CodeIgniter
-Creating CV with CodeIgniter
---
+++ application/views/tampilan_utama.php
@@ -0,0 +1,227 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Resume - Start Bootstrap Theme</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Anggara Yuda Pratama</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/angga.png" alt="..." /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                    <!-- <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li> -->
+                </ul>
+            </div>
+        </nav>
+
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Anggara
+                        <span class="text-primary">Yuda Pratama</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Manyar Tirto Asri III · 081234567809 ·
+                        <a href="mailto:angga@gmail.com">angga@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">Pria berusia  20 tahun yang lahir di Surabaya. Sangat ambisius untuk mempelajari hal-hal baru dalam semua kegiatan dan siap menjadi lebih produktif dan mendapatkan pengalaman bekerja di bidang yang saya mampu. Suka dalam hal videografi.</p>
+                    <div class="social-icons">
+                        <!-- <a class="social-icon" href="#!"><i class="fab fa-linkedin-in"></i></a> -->
+                        <a class="social-icon" href="https://twitter.com/anggarayp2"><i class="fab fa-twitter"></i></a>
+                        <a class="social-icon" href="https://www.instagram.com/anggarayp/"><i class="fab fa-instagram"></i></a>
+                        <a class="social-icon" href="https://github.com/anggarayp"><i class="fab fa-github"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Editor Tutorial Tes Kemampuan Akademik ITS</h3>
+                            <div class="subheading mb-3">Editor video dari tutorial TKA ITS</div>
+                            <p>Mengedit dan menambah beberapa bagian video tutorial dari tahun sebelumnya. Hal ini supaya tahun selanjutnya cukup pakai satu video aja.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Mei 2021 - Juni 2021</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Podcast Officer</h3>
+                            <div class="subheading mb-3">Magang di Rumah Sandyakala</div>
+                            <p>Magang di Rumah Sandyakala sebagai podcast officer. Podcast officer ini bertugas menjadi editor dari podcast yang dibuat oleh Rumah Sandyakala. Podcast ini nanti tayang di spotify milik Rumah Sandyakala.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Juni 2011 - Agustus 2021</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Front End</h3>
+                            <div class="subheading mb-3">Mendesain Web</div>
+                            <p>Membuat dan mendesain web dari BUMdes Mina Harapan. Dengan rekan saya, Clement Prolifel mendesain ulang web milik BUMdes Mina Harapan. Web ini berisi informasi potensi alam yang dimiliki oleh BUMdes Mina Harapan dan marketplace produk yang juga dimiliki oleh BUMdes Mina Harapan.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">April 2020 - Juni 2021</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">SD Negeri Kertajaya</h3>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2006 - 2012</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">SMPN 1 Surabaya</h3>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2012 - 2015</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">SMAN 5 Surabaya</h3>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2015 - May 2018</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Institut Teknologi Sepuluh Nopember</h3>
+                            <div class="subheading mb-3">Informatics Engineering</div>
+                            <!-- <div>Computer Science - Web Development Track</div>
+                            <p>GPA: 3.23</p> -->
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2018 - sampai sekarang</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Hal yang dipelajari dan dikuasai</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fas fa-film"></i></li>
+                        <li class="list-inline-item"><i class="fas fa-video"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-bootstrap"></i></li>
+                    </ul>
+                    <!-- <div class="subheading mb-3">Workflow</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Mobile-First, Responsive Design
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Browser Testing & Debugging
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Functional Teams
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Agile Development & Scrum
+                        </li>
+                    </ul> -->
+                </div>
+            </section>
+            <hr class="m-0" />
+
+            <!-- Interests-->
+            <section class="resume-section" id="interests">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Interests</h2>
+                    <p>Selain dalam bidang informatika, saya sangat menyukai dalam bidang videografi. Selain itu, juga sangat suka travelling dan bermain futsal. Hobi nonton sepak bola dan basket juga.</p>
+                    <p class="mb-0">Ketika di rumah saya suka bermain game, baik game pc maupun console. Dalam free-time suka eksplore tentang kopi.</p>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Awards-->
+            <!-- <section class="resume-section" id="awards">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Awards & Certifications</h2>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Google Analytics Certified Developer
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Mobile Web Specialist - Google Certification
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            Place - University of Colorado Boulder - Emerging Tech Competition 2009
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            2
+                            <sup>nd</sup>
+                            Place - University of Colorado Boulder - Emerging Tech Competition 2008
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            1
+                            <sup>st</sup>
+                            Place - James Buchanan High School - Hackathon 2006
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            3
+                            <sup>rd</sup>
+                            Place - James Buchanan High School - Hackathon 2005
+                        </li>
+                    </ul>
+                </div>
+            </section> -->
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
